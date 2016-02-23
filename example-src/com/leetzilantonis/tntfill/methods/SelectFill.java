9a1f5bb55cad55394fc89bd4c9a26679ce73dcee
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-02-23 16:21:30 +1100

    Create SelectFill.java

---
+++ example-src/com/leetzilantonis/tntfill/methods/SelectFill.java
@@ -0,0 +1,14 @@
+package com.leetzilantonis.tntfill;
+
+import java.util.LinkedList;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class SelectFill {
+
+	public FillResult fillSelectedDispensersSurvival(Player p, LinkedList<Location> locations, int tntPer, int tntRemoved) {}
+	
+	public FillResult fillSelectedDispensersCreative(Player p, LinkedList<Location> locations, int tntPer) {}
+
+}
