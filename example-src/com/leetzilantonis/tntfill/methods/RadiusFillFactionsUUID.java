8554234110ef4a78f9958bdb7e6a9c90ca8b4270
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-02-23 16:20:56 +1100

    Create RadiusFillFactionsUUID.java

---
+++ example-src/com/leetzilantonis/tntfill/methods/RadiusFillFactionsUUID.java
@@ -0,0 +1,19 @@
+package com.leetzilantonis.tntfill;
+
+import org.bukkit.entity.Player;
+
+public class RadiusFillFactionsUUID {
+
+	Main plugin;
+
+	public RadiusFillFacitonsUUID(Main plugin){
+		this.plugin = plugin;
+	}
+
+	public int countDispensers(Player p, int radius) {}
+
+	public FillResult fillDispensersSurvival(Player p,  int radius, int tntPer, int tntRemoved) {}
+
+	public FillResult fillDispensersCreative(Player p,  int radius, int tntPer) {}
+
+}
