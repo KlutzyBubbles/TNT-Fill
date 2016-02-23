7cbe29d6583f6a1326dc3129937261fcf02a102d
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-02-23 16:24:27 +1100

    Create ClearResult.java

---
+++ example-src/com/leetzilantonis/tntfill/ClearResult.java
@@ -0,0 +1,14 @@
+package com.leetzilantonis.tntfill;
+
+public enum ClearResult {
+
+	PART_CLEARED,
+	CLEARED,
+	NO_TNT,
+	UNKNOWN,
+	PART_CLEARED_FACTION,
+	CLEARED_FACTION,
+	NO_TNT_FACTION,
+	NO_SPACE
+	
+}
