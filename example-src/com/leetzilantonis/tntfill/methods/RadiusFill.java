20900736efa82d366f024e969873a4e2b1f44dda
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-02-23 16:20:22 +1100

    Create RadiusFill.java

---
+++ example-src/com/leetzilantonis/tntfill/methods/RadiusFill.java
@@ -0,0 +1,19 @@
+package com.leetzilantonis.tntfill;
+
+import org.bukkit.entity.Player;
+
+public class RadiusFill {
+
+	Main plugin;
+
+	public RadiusFill(Main plugin){
+		this.plugin = plugin;
+	}
+
+	public int countDispensers(Player p, int radius) {}
+
+	public FillResult fillDispensersSurvival(Player p,  int radius, int tntPer, int tntRemoved) {}
+
+	public FillResult fillDispensersCreative(Player p,  int radius, int tntPer) {}
+
+}
